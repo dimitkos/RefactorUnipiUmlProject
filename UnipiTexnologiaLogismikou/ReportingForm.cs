49e2d5c201a2e6050e5c5d66d312111ed65013f9
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-04-30 23:50:24 +0300

    ebala thn μεθοδο countframework  στον αντμιν καισ το αντιστοιχο φορμ

--- UnipiTexnologiaLogismikou/ReportingForm.cs
+++ UnipiTexnologiaLogismikou/ReportingForm.cs
@@ -15,7 +15,7 @@ namespace UnipiTexnologiaLogismikou
     public partial class ReportingForm : Form
     {
         static string myconnstring = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
-
+        Admin admin = new Admin();
         public ReportingForm()
         {
             InitializeComponent();
@@ -23,74 +23,18 @@ namespace UnipiTexnologiaLogismikou
 
         private void button1_Click(object sender, EventArgs e)
         {
-            DataTable dt = Select(textBox1.Text,textBox2.Text);
+            DataTable dt = admin.ShowGradesRange(textBox1.Text,textBox2.Text);
             dataGridView1.DataSource = dt;
         }
 
-        private  DataTable Select(string t1 ,string t2)
-        {
-            SqlConnection con = new SqlConnection(myconnstring);
-
-            DataTable dt = new DataTable();
-            try
-            {
-                String sql = "SELECT st.id ,st.namesurname,gr.grades FROM studentinfo as st, grade as gr " +
-                    " where st.id=gr.id and gr.grades>=@t1 and gr.grades<=@t2";
-
-                SqlCommand cmd = new SqlCommand(sql, con);
-                cmd.Parameters.AddWithValue("@t1",t1 );
-                cmd.Parameters.AddWithValue("@t2", t2);
-                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-                con.Open();
-                adapter.Fill(dt);
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                con.Close();
-            }
-            return dt;
-        }
+        
 
         private void frameworkbutton_Click(object sender, EventArgs e)
         {
-            var result = SelectFramework(textBox3.Text.Trim());
+            var result = admin.CountFrameworks(textBox3.Text.Trim());
             label4.Text = result.ToString();
         }
 
-        private int SelectFramework(string framework)
-        {
-            SqlConnection con = new SqlConnection(myconnstring);
-
-            int result = 0;
-            try
-            {
-                String sql = "SELECT Count(*) FROM studentinfo as st " +
-                    " where st.framework=@framework";
-
-                SqlCommand cmd = new SqlCommand(sql, con);
-                cmd.Parameters.AddWithValue("@framework", framework);
-                
-               
-                con.Open();
-                result = Convert.ToInt32(cmd.ExecuteScalar());
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                con.Close();
-            }
-
-            return result;
-        }
 
         private void ReportingForm_Load(object sender, EventArgs e)
         {
