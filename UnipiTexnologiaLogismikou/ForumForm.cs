c7b472e5dcf60c80873463d5f473eb4a3ccf7c46
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-05-01 01:06:33 +0300

    eftiaja kai sthn class studen methodo na diavazei mnm

--- UnipiTexnologiaLogismikou/ForumForm.cs
+++ UnipiTexnologiaLogismikou/ForumForm.cs
@@ -39,45 +39,8 @@ namespace UnipiTexnologiaLogismikou
 
         private void enterbutton_Click(object sender, EventArgs e)
         {
-            DateTime date = DateTime.Now;
-            string formattedDate = date.ToString("dddd, dd MMMM yyyy HH:mm");
-            //στην συνεχεια θα κανουμε append  σε ενα αρχειο . αν αυτο το αρχειο δεν υπαρχιε θα το δημιουργησει
-
-                using (StreamWriter sw1 = File.AppendText("comments.txt"))
-                {
-                    //ελεγχει αν το ονομα του χρηστη ειναι κενο,και του δινει το text  ανωνυμος
-                    if (textBox1.Text == "")
-                    {
-                        textBox1.Text = "Anonymous";
-                    }
-                    //στην περιπτωση ομως που το text  του σχολειου ειναι κενο του λεει οτι πρεπει να γραψει μηνυμα
-                    //αν ολα ειναι οκ τοτε φραφει το μηνυμα
-                    if (textBox2.Text == "")
-                    {
-                        MessageBox.Show("you must type a comment");
-                    }
-                    else
-                    {
-                        //στην συνεχεια του λεμε να γράψει τα καταλληλα μηνυμα,σε περιπτωση που παραχθει καποιο
-                        //exception  βγαινουν τα αντιστοιχα μηνυματα
-                        try
-                        {
-                            sw1.WriteLine("---------------------------------------------------------------");
-                            sw1.WriteLine("Ο χρήστης " + textBox1.Text + " την ημερομηνία " + formattedDate + " σχολίασε: ");
-                            sw1.WriteLine(textBox2.Text);
-                            MessageBox.Show("Το σχόλιο σας υποβλήθηκε επιτυχώς");
-                        }
-                        catch (IOException e1)
-                        {
-                            MessageBox.Show(e1.GetType().Name);
-                        }
-                        catch (ObjectDisposedException e2)
-                        {
-                            MessageBox.Show("Object Disposed Exception");
-                        }
-                    }
-
-                }
+            Student st = new Student();
+            st.WriteCommentStudent(textBox1, textBox2);
             
         }
 
@@ -86,26 +49,8 @@ namespace UnipiTexnologiaLogismikou
             //με το πατημα του κουμπιου εμφανιζεται το panel1  οπου περιεχει το textbox3 οπου θα  εμφανισιε το περιεχομενο 
             //του txt αρχειου οπου καταχωρουνται τα σχολια
             panel1.Show();
-            try
-            {
-                StringBuilder sb = new StringBuilder();
-                using (StreamReader sr = new StreamReader("comments.txt"))
-                {
-                    while (sr.Peek() >= 0)
-                    {
-                        sb.AppendLine(sr.ReadLine());
-                    }
-                    textBox3.Text = sb.ToString();
-                }
-            }
-            catch (IOException e3)
-            {
-                MessageBox.Show(e3.GetType().Name);
-            }
-            catch (ArgumentException e4)
-            {
-                MessageBox.Show(e4.GetType().Name);
-            }
+            Student st = new Student();
+            st.ReadCommentStudent(textBox3);
         }
 
         private void textBox3_TextChanged(object sender, EventArgs e)
--- UnipiTexnologiaLogismikou/Student.cs
+++ UnipiTexnologiaLogismikou/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
@@ -81,6 +82,73 @@ namespace UnipiTexnologiaLogismikou
                 uploadbutton.Text = "Thesis Uploded";
                 MessageBox.Show("Thesis Uploded Successfully");
             };
+        }
+
+        public void WriteCommentStudent(TextBox textBox1, TextBox textBox2)
+        {
+            DateTime date = DateTime.Now;
+            string formattedDate = date.ToString("dddd, dd MMMM yyyy HH:mm");
+            //στην συνεχεια θα κανουμε append  σε ενα αρχειο . αν αυτο το αρχειο δεν υπαρχιε θα το δημιουργησει
+
+            using (StreamWriter sw1 = File.AppendText("comments.txt"))
+            {
+                //ελεγχει αν το ονομα του χρηστη ειναι κενο,και του δινει το text  ανωνυμος
+                if (textBox1.Text == "")
+                {
+                    textBox1.Text = "Anonymous";
+                }
+                //στην περιπτωση ομως που το text  του σχολειου ειναι κενο του λεει οτι πρεπει να γραψει μηνυμα
+                //αν ολα ειναι οκ τοτε φραφει το μηνυμα
+                if (textBox2.Text == "")
+                {
+                    MessageBox.Show("you must type a comment");
+                }
+                else
+                {
+                    //στην συνεχεια του λεμε να γράψει τα καταλληλα μηνυμα,σε περιπτωση που παραχθει καποιο
+                    //exception  βγαινουν τα αντιστοιχα μηνυματα
+                    try
+                    {
+                        sw1.WriteLine("---------------------------------------------------------------");
+                        sw1.WriteLine("Ο χρήστης " + textBox1.Text + " την ημερομηνία " + formattedDate + " σχολίασε: ");
+                        sw1.WriteLine(textBox2.Text);
+                        MessageBox.Show("Το σχόλιο σας υποβλήθηκε επιτυχώς");
+                    }
+                    catch (IOException e1)
+                    {
+                        MessageBox.Show(e1.GetType().Name);
+                    }
+                    catch (ObjectDisposedException e2)
+                    {
+                        MessageBox.Show("Object Disposed Exception");
+                    }
+                }
+
+            }
+        }
+
+        public void ReadCommentStudent(TextBox textBox3)
+        {
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                using (StreamReader sr = new StreamReader("comments.txt"))
+                {
+                    while (sr.Peek() >= 0)
+                    {
+                        sb.AppendLine(sr.ReadLine());
+                    }
+                    textBox3.Text = sb.ToString();
+                }
+            }
+            catch (IOException e3)
+            {
+                MessageBox.Show(e3.GetType().Name);
+            }
+            catch (ArgumentException e4)
+            {
+                MessageBox.Show(e4.GetType().Name);
+            }
         }
     }
 }
